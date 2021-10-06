65b6751c584a13db596d4f1ca66297fe9acca285
BandaVallary <60439564+BandaVallary@users.noreply.github.com>
2021-10-06 16:45:08 +0300

    Trying to deploy my chatbot with the heroku server

--- actions/actions.py
+++ actions/actions.py
@@ -5,23 +5,37 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+# This is an example for creating a form
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ValidateRestaurantForm(Action):
+    def name(self) -> Text:
+        return "user_details_form"
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["name", "number"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # this slot is not filled, therefore rquest the user to fill it next
+                return[SlotSet("requested_slot", slot_name)]
+                # if all slots are filled
+        return[SlotSet ("requested_slot", None)]
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+    def run( self, 
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, any]]:
+        dispatcher.utter_message(template= "utter_details_thanks",
+        Name= tracker.get_slot("name"), Phone_number= tracker.get_slot("number") )
+
+
